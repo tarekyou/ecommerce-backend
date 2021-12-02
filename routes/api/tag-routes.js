d739d7ca886fd45ba9a64f5b5c0d7d88c62d05e5
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-12-01 18:53:24 -0800

    debugged tags routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
         through: ProductTag,
-        as: 'products',
+        as: 'description',
       },
     ],
   })
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
         through: ProductTag,
-        as: 'products',
+        as: 'description',
       },
     ],
   })
